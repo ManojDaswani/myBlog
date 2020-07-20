1ef44cffdc8c7118364100674de951f0b3b936c9
Manoj <ManojDaswani@users.noreply.github.com>
2020-07-20 18:29:28 +0530

    Update gallery.js

--- repo/gatsby-bootcamp/src/pages/gallery.js
+++ repo/gatsby-bootcamp/src/pages/gallery.js
@@ -24,7 +24,7 @@ const Gallery = ({data}) => (
         
         {/* using the components below */}
         <Img fluid={
-            data.gatsbyXXX.childImageSharp.fluid} alt="ManojId" style={{ width: 150, float: "right", marginLeft: 20, marginBottom: 10 }} />
+            data.gatsbyXXX.childImageSharp.fluid} alt="XXX" style={{ width: 150, float: "right", marginLeft: 20, marginBottom: 10 }} />
 
             <p> Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. </p>
         <Img fixed={
